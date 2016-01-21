b019c20dc63e4ac7be0bd9b5bd00e3af72091f32
jbf81tb <jbf81tb@gmail.com>
2016-01-21 14:55:10 -0500

    comb_run overhaul

--- comb_run.m
+++ comb_run.m
@@ -1,34 +1,103 @@
-function comb_run(exp_name)
-% Prior to running this there should be a directory of the form
-%  '~/experiment_name/orig_movies'
-% and inside the orig_movies folder should be the original tifs
-% Also, you must include a background movie even if the background
-% has already been removed. If that is the case then the background movie
-% will be ignored, but variables still care about it and would error out
-% if it weren't there.
-% REMINDER: '~' is the home directory, something like '/nfs/##/osu####/'
-%           Basically, it's where you start out.
-
-omd = fullfile(exp_name,'orig_movies'); %experiment directory (should use this templating)
-smd = fullfile(exp_name,'split_movies');
-
+function comb_run(exp_name,varargin)
+%COMB_RUN Run cmeAnalysis on multiple files.
+% Takes 5 arguments.
+% 1) Path to experiment folder (string). Experiment folder should contain the folder
+% "orig_movies" and that should contain all of the movies you want to run
+% over.
+% 2) Framegap of the movies.
+% 3) Threshold to apply to movies.
+% 4) Number of sections the movie will be divided into. Default section
+% size is 500, so if the movie is longer than 500 frames it will be more
+% than 1 section.
+% 4) Section size. This variable exists for memory considerations. 
+%
+% Any argument may be a scalar or vector. Use a scalar if all the movies
+% share the necessary properties. Use a vector if you need to specify
+% particular properties for particular movies. The movies work in the order
+% of the 'dir' function, which can be different from the order that the
+% files are in the file explorer, especially if the filenames end in
+% numbers of different digit lengths.
+%% Find out how many movies there are.
+omd = fullfile(exp_name,'orig_movies');
 tmpd = dir(fullfile(omd,'*.tif'));
 movies      = cell(length(tmpd),1);
+%% Input checking
+if nargin==1
+    framegap = 1*ones(length(movies),1);
+    Threshs = 400*ones(length(movies),1);
+    sections = 1*ones(length(movies),1);
+    sectionsize = 500;
+elseif nargin==2
+    if length(varargin{1})>1
+        framegap = varargin{1};
+    elseif  isscalar(varargin{1})
+        framegap = varargin{1}*ones(length(movies),1);
+    end
+    Threshs = 400*ones(length(movies),1);
+    sections = 1*ones(length(movies),1);
+    sectionsize = 500;
+elseif nargin==3
+    if length(varargin{1})>1
+        framegap = varargin{1};
+    elseif  isscalar(varargin{1})
+        framegap = varargin{1}*ones(length(movies),1);
+    end
+    if length(varargin{2})>1
+        Threshs = varargin{2};
+    elseif  isscalar(varargin{2})
+        Threshs = varargin{2}*ones(length(movies),1);
+    end
+    sections = 1*ones(length(movies),1);
+    sectionsize = 500;
+elseif nargin==4
+    if length(varargin{1})>1
+        framegap = varargin{1};
+    elseif  isscalar(varargin{1})
+        framegap = varargin{1}*ones(length(movies),1);
+    end
+    if length(varargin{2})>1
+        Threshs = varargin{2};
+    elseif  isscalar(varargin{2})
+        Threshs = varargin{2}*ones(length(movies),1);
+    end
+    if length(varargin{3})>1
+        sections = varargin{3};
+    elseif  isscalar(varargin{3})
+        sections = varargin{3}*ones(length(movies),1);
+    end
+    sectionsize = 500;
+elseif nargin==5
+    if length(varargin{1})>1
+        framegap = varargin{1};
+    elseif  isscalar(varargin{1})
+        framegap = varargin{1}*ones(length(movies),1);
+    end
+    if length(varargin{2})>1
+        Threshs = varargin{2};
+    elseif  isscalar(varargin{2})
+        Threshs = varargin{2}*ones(length(movies),1);
+    end
+    if length(varargin{3})>1
+        sections = varargin{3};
+    elseif  isscalar(varargin{3})
+        sections = varargin{3}*ones(length(movies),1);
+    end
+    sectionsize = varargin{4};
+end
+%% Making folders
+smd = fullfile(exp_name,'split_movies');
+if ~exist(smd,'dir')
+    mkdir(smd);
+end
 splitmovies = cell(length(tmpd),1);
-
-%%%%%%%% Adjust %%%%%%%%
-framegap = 5*ones(length(movies),1);
-Threshs = 1000*ones(length(movies),1);
-sectionsize = 350;
-sections = 1*ones(length(movies),1);
-%%%%%%%%%%%%%%%%%%%%%%%%
 
 for i = 1:length(movies)
     movies{i} = fullfile(omd,tmpd(i).name);
     splitmovies{i} = fullfile(smd,tmpd(i).name(1:(end-4)));
     mkdir(splitmovies{i});
 end
-
+%% Actual Execution
 LongMultiMovieSplitAnalysis(movies,sectionsize,splitmovies,framegap);
+clear functions
 LongMoviePostCME_osc(smd,omd,Threshs,sections);
 end
